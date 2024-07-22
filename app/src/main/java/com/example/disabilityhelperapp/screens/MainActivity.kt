3cad4eda4a469ec66562e1c4c45177b0e42af3c6
Emile Oosthuizen <97406581+Tuscansun1@users.noreply.github.com>
2024-07-22 23:33:46 +0200

    License Update

--- app/src/main/java/com/example/disabilityhelperapp/screens/MainActivity.kt
+++ app/src/main/java/com/example/disabilityhelperapp/screens/MainActivity.kt
@@ -70,3 +70,110 @@ fun DefaultPreview() {
         MyScreen() // Preview the screen composable
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ * Custom MIT License for DisabilityHelperAPP
+ *
+ * Copyright (c) 2024 Emile B. Oosthuizen
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, subject to the following conditions:
+ *
+ * 1. Attribution: The above copyright notice and this permission notice must be included in all copies
+ *    or substantial portions of the Software.
+ *
+ * 2. Monetization Restrictions:
+ *    - You may use, modify, and distribute the Software for non-commercial purposes without restriction.
+ *    - For any commercial use or distribution, you must obtain written permission from the original author (Emile B. Oosthuizen).
+ *
+ * 3. Liability Disclaimer: The Software is provided "as is", without warranty of any kind. In no event shall
+ *    the authors or copyright holders be liable for any claim, damages, or other liability, whether in an action
+ *    of contract, tort, or otherwise, arising from, out of, or in connection with the Software or the use or
+ *    other dealings in the Software.
+ *
+ * 4. Termination: This license is effective until terminated. You may terminate it at any time by destroying all
+ *    copies of the Software. The license will terminate automatically if you fail to comply with any of its terms.
+ *
+ * 5. Governing Law: This license shall be governed by and construed in accordance with the laws of [South Africa].
+ */
+
